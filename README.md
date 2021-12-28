22a35e8ab638d9d887b81e21c6c571cc80daccda
AdalZanabria <adal.zanabria@gmail.com>
2021-12-28 21:27:39 +0000

    Se modificó el README y se crearon los archivos figuras en html y js

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Javascript-Practica
+
+## Taller No.1 de JavaScript: Figuras Geométricas
+
+- Primer paso: Definir las fórmulas. 🧑‍🏫
+- Segundo paso: Implementar las fórmulas en JavaScript. 🧮
+- Tercer paso: Crear funciones. 📐
+- Cuarto paso: Integrar JavaScript con HTML. 🖥️
---
+++ figuras.js
@@ -0,0 +1,8 @@
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado);
+
+const areaCuadrada = ladoCuadrado * ladoCuadrado;
+console.log("");
