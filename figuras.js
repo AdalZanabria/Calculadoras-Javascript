d3cb31937da3ff6851dcb9ea44e4653f4fa4c472
AdalZanabria <adal.zanabria@gmail.com>
2021-12-29 03:02:59 +0000

    Se terminó el cuadrado y se agregó el triángulo al archivo figuras.js

--- figuras.js
+++ figuras.js
@@ -1,8 +1,32 @@
+// Código del cuadrado
+console.group("Cuadrado");
+
 const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm.");
 
 const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado);
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm.");
 
 const areaCuadrada = ladoCuadrado * ladoCuadrado;
-console.log("");
+console.log("El área del cuadrado es: " + areaCuadrada + "cm².");
+
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulo");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "Los lados del triángulo miden: "
+    + ladoTriangulo1
+    + "cm, "
+    + ladoTriangulo2
+    + "cm y "
+    + baseTriangulo
+    + "cm."
+    );
+
+console.groupEnd();
