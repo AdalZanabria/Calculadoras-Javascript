56a738da47df4f985b1d28fe61e0e6b607499d0f
AdalZanabria <adal.zanabria@gmail.com>
2022-01-02 03:18:14 -0600

    Se agregó navbar y se creó un archivo que controla la visibilidad de las diferentes calculadoras

---
+++ navigationDisplay.js
@@ -0,0 +1,16 @@
+//Calculadora Geométrica
+function display1() {
+    calculadora2 = document.getElementById("calculadoraDescuentos");
+    calculadora2.style.display = "none";
+
+    calculadora1 = document.getElementById("calculadoraGeometrica");
+    calculadora1.style.display = "block";
+}
+
+function display2() {
+    calculadora1 = document.getElementById("calculadoraGeometrica");
+    calculadora1.style.display = "none";
+
+    calculadora2 = document.getElementById("calculadoraDescuentos");
+    calculadora2.style.display = "block";
+}
