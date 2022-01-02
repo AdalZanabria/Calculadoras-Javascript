949533746ea32ac930b1c86a37bcb957b530c709
AdalZanabria <adal.zanabria@gmail.com>
2022-01-02 03:58:52 -0600

    Se puso la información del formulario péro falta estilizar y programar el modal de resultado

--- descuentos.js
+++ descuentos.js
@@ -4,3 +4,33 @@ function calcularPrecioConDescuento(precio, descuento) {
 
     return precioConDescuento;
 }
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = Number(inputPrice.value);
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = Number(inputDiscount.value);
+
+    const precioConDescuento = calcularPrecioConDescuento(
+        priceValue,
+        discountValue
+    );
+    return precioConDescuento;
+}
+
+// Modal
+var ventanaModal = document.getElementById("ventanaModal");
+ventanaModal.addEventListener("show.bs.modal", function (event) {
+    // Button that triggered the modal
+    var button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    var calculo = button.getAttribute("data-bs-calculo");
+
+    // Update the modal's content.
+    var modalTitle = ventanaModal.querySelector(".modal-title");
+    var modalBody = ventanaModal.querySelector(".modal-body");
+
+    modalTitle.textContent = "El precio con descuento es de:";
+    modalBody.textContent =
+        "El " + calculo + " del " + figura + " es igual a: " + resultado;
+});
