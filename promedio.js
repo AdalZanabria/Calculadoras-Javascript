5641508ee6ae9f3f8146c4e9552348e37a1cafbd
AdalZanabria <adal.zanabria@gmail.com>
2022-01-07 00:32:05 -0600

    Se creó la función para calcular la mediana en promedio.js, falta la moda.

--- promedio.js
+++ promedio.js
@@ -12,10 +12,6 @@ function calcularMediaAritmetica(lista) {
 }
 
 //Mediana
-const lista1 = [100, 200, 500, 400000000];
-
-const mitadLista1 = parseInt(lista1.length / 2);
-
 function esPar(numero) {
     if (numero % 2 === 0) {
         return true;
@@ -24,13 +20,30 @@ function esPar(numero) {
     }
 }
 
-let mediana;
+function calcularMediana(lista) {
+    //Ordenar la lista de menor a mayor
+    let listaOrdenada = lista.sort(function (a, b) {
+        return a - b;
+    });
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1 - 1];
-    const elemento2 = lista1[mitadLista1];
-    const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
-    mediana = promedioElemento1y2;
-} else {
-    mediana = lista1[mitadLista1];
+    // Calcular la mited de la lista dividiendo su longitud entre 2, y cambiarlo a Int en caso de que de un número decimal
+    const mitadLista = parseInt(listaOrdenada.length / 2);
+
+    let mediana;
+
+    // Si es par, calcular la media aritmética de los dos números que se encuentren en medio de la lista
+    if (esPar(listaOrdenada.length)) {
+        const elemento1 = listaOrdenada[mitadLista - 1];
+        const elemento2 = listaOrdenada[mitadLista];
+        const promedioElemento1y2 = calcularMediaAritmetica([
+            elemento1,
+            elemento2,
+        ]);
+        mediana = promedioElemento1y2;
+    } else {
+        //Si no es par, retornar el elemento en la mitad de la lista
+        mediana = listaOrdenada[mitadLista];
+    }
+
+    return mediana;
 }
