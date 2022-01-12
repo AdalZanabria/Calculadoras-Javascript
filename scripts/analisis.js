9a165b348b2fee9f82b9bfe1a83bb00e4189ea1a
AdalZanabria <adal.zanabria@gmail.com>
2022-01-11 21:08:25 -0600

    Cartas de personas dinámicas empiezan en dos y muestran correctamente su número de persona con id único.

--- scripts/analisis.js
+++ scripts/analisis.js
@@ -1,30 +1,25 @@
 //Helpers
-const salariosCol = colombia.map(
-    function(persona){
-        return persona.salary;
-    }
-);
+const salariosCol = colombia.map(function (persona) {
+    return persona.salary;
+});
 
-const salariosColSorted = salariosCol.sort(
-    function(salaryA, salaryB){
-        return salaryA - salaryB;
-    }, 0
-);
+const salariosColSorted = salariosCol.sort(function (salaryA, salaryB) {
+    return salaryA - salaryB;
+}, 0);
 
 // Mediana de salarios
-function medianaSalarios (lista) {
+function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2);
 
-    if (esPar(lista.length)){
-        personaMitad1 = lista[mitad-1];
+    if (esPar(lista.length)) {
+        personaMitad1 = lista[mitad - 1];
         personaMitad2 = lista[mitad];
-        return (personaMitad1+personaMitad2) / 2;
-
-    }else {
+        return (personaMitad1 + personaMitad2) / 2;
+    } else {
         personaMitad = lista[mitad];
         return personaMitad;
     }
-};
+}
 
 const medianaGeneralCol = medianaSalarios(salariosColSorted);
 
@@ -35,6 +30,7 @@ const salariosColTop10 = salariosColSorted.splice(spliceStart, spliceCount);
 const medianaTop10Col = medianaSalarios(salariosColTop10);
 
 // Personas Dinámicas
+var numeroPersona = 2;
 const personasContainer = document.getElementById("personasContainer");
 const agregarPersona = document.getElementById("agregarPersona");
 const eliminarPersona = document.getElementById("eliminarPersona");
@@ -43,13 +39,21 @@ agregarPersona.onclick = function () {
     var personaDiv1 = document.querySelector("#personaDiv1");
     var nuevaPersona = personaDiv1.cloneNode(true);
 
+    numeroPersona++;
+
+    var span = nuevaPersona.getElementsByClassName("numeroPersona")[0];
+    span.innerText = numeroPersona;
+
+    nuevaPersona.id = "personaDiv" + numeroPersona;
     personasContainer.appendChild(nuevaPersona);
 };
 
 eliminarPersona.onclick = function () {
+    if (numeroPersona > 2) {
+        numeroPersona--;
+    }
     var personasDivs = personasContainer.getElementsByClassName("personaDiv");
-    if (personasDivs.length > 1) {
+    if (personasDivs.length > 2) {
         personasContainer.removeChild(personasDivs[personasDivs.length - 1]);
     }
 };
-
