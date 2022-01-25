3776a4cb2c3d625ec82567441bbab91fc34cfc70
AdalZanabria <adal.zanabria@gmail.com>
2022-01-25 16:00:45 -0600

    Dividí los archivos de configuración de webpack en prod y dev para usar el server, comencé a hacer los íconos de certificados.

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -9,6 +9,19 @@ const Header = () => {
                 <p class="lead text-center" id="headerSubtitle">
                     Selecciona un tipo de calculadora desde el menu superior
                 </p>
+                
+                <div class="container d-flex flex-column align-items-center" id="certificados">
+                    <hr>
+                    <p class="text-center">Este sitio fue creado como parte de mis siguientes certificados:</p>
+                    <p class="small text-center">Haz click en cada categoría para ver los certificados</p>
+                    <div class="row">
+                        <div class="col-md-3">HTML y CSS</div>
+                        <div class="col-md-3">JavaScript</div>
+                        <div class="col-md-3">Webpack</div>
+                        <div class="col-md-3">SPA</div>
+                    </div>
+                </div>
+                
             `;
 
     return view;
